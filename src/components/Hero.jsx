bdf2fa686ff4fec202efda2f7447459b62b84451
Prakash9399 <rajputprakash9399@gmail.com>
2026-08-03 13:14:39 +0530

    feat: image section updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import {HERO_CONTENT} from "../constants"
-import profilePic from "../assets/kevinRushProfile.png"
+import heroImage from "../assets/image.png"
 import { motion } from "framer-motion"
 
 const  container=(delay)=>({
@@ -41,7 +41,7 @@ const Hero = () => {
                   <motion.img initial={{x:100,opacity:0}}
                   animate={{x:0,opacity:1}}
                   transition={{duration:1,delay:1.2}}
-                  src={profilePic} alt="Profile" />
+                  src={heroImage} alt="Profile" />
               </div>
         </div>
       </div>
